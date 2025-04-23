8659d8a2c497b6ee90c9ccd244ecbf999586e081
titos alan notebook <tbeatzz20k@gmail.com>
2025-04-23 15:58:45 -0300

    tp3 terminado

---
+++ TP_04/js/script.js
@@ -0,0 +1,28 @@
+const mostrarError = ()=> console.error("un msj de error");
+// = () => 
+
+const sumar = (x,y) => (x+y);
+
+const cuadrado = x => (x*x);
+
+const alumno = {
+    apellido: "titos",
+    localidad: "caleta olivia",
+    edad: 21,
+    calcularEdad: ()=> {
+        //calc edad
+    },
+
+    save: ()=>{},
+    update: ()=>{},
+    delete: ()=> {}
+};
+
+const getAlumno = id => ({
+    apellido: "titos",
+    localidad: "caleta olivia",
+    
+})
+// ARREGLOS 
+
+let colores = new Array(5);
