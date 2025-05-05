336da30e469e135c030a5b7234b19540a926653d
titosalan <tbeatzz20k@gmail.com>
2025-05-05 13:12:10 -0300

    Update main.js

--- TP_04/ejercicio_07/main.js
+++ TP_04/ejercicio_07/main.js
@@ -5,23 +5,21 @@
 // d) Mostrar las notas multiplicadas por 10 con map (para simular escalado de puntajes).
 
 
-let notas = [4,6,10,7,8,2,9,1,5];
-for(let i = 0; i< notas.length; i++){
-    console.log("nota de la materia nro ",i ," : ", notas[i])
+let notas = [4, 6, 10, 7, 8, 2, 9, 1, 5];
+
+// a) Mostrar todas las notas usando el ciclo for.
+for (let i = 0; i < notas.length; i++) {
+    console.log("Nota de la materia nro", i, ":", notas[i]);
 }
 
+// b) Calcular el promedio usando reduce.
+let promedio = notas.reduce((acum, nota) => acum + nota, 0) / notas.length;
+console.log("Promedio:", promedio);
 
-function calcPromedio(arreglo, cantNotas=arreglo.length){
-    let promedio=0;
-    for(let i=0; i<cantNotas; i++){
-        promedio += arreglo[i];
+// c) Crear un nuevo array con las notas aprobadas (mayores o iguales a 6).
+let notasAprobadas = notas.filter(nota => nota >= 6);
+console.log("Notas aprobadas:", notasAprobadas);
 
-        console.log(promedio, arreglo[i]);
-    }
-
-    return promedio/cantNotas;
-}
-
-
-
-
+// d) Mostrar las notas multiplicadas por 10 con map.
+let notasEscaladas = notas.map(nota => nota * 10);
+console.log("Notas escaladas:", notasEscaladas);
